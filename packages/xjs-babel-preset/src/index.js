c09501eecdba4e95a20a6e6bcb22ba6154092f7f
Issa Tseng <maintainer@heyissa.com>
2019-07-11 17:31:18 -0700

    xjs: turn on a bunch of es proposals.

--- packages/xjs-babel-preset/src/index.js
+++ packages/xjs-babel-preset/src/index.js
@@ -1,5 +1,11 @@
 import { declare } from "@babel/helper-plugin-utils";
+
 import pipeline from "@babel/plugin-proposal-pipeline-operator";
+import functionBind from "@babel/plugin-proposal-function-bind";
+import optionalChaining from "@babel/plugin-proposal-optional-chaining";
+import nullishCoalesce from "@babel/plugin-proposal-nullish-coalescing-operator";
+import doExpr from "@babel/plugin-proposal-do-expressions";
+
 import guard from "@xjs/plugin-transform-guard";
 import accessAsFunction from "@xjs/plugin-transform-access-as-function";
 
@@ -8,7 +14,14 @@ export default declare(
     api.assertVersion(7);
 
     return { overrides: [{ plugins: [
-      [ pipeline, { "proposal": "fsharp" } ],
+      // extant es proposal plugins:
+      [ pipeline, { proposal: "fsharp" } ],
+      [ functionBind ],
+      [ optionalChaining, { loose: true } ],
+      [ nullishCoalesce, { loose: true } ],
+      [ doExpr ],
+
+      // xjs plugins:
       [ guard ],
       [ accessAsFunction ],
     ] }] };
