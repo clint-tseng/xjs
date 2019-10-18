e5d0a4cf8fad4a7f4d055d50115d69cdeda7170a
Issa Tseng <maintainer@heyissa.com>
2019-10-18 01:22:33 -0700

    xjs: invert existential operator. not sure what i was thinking.

--- compiler/packages/xjs-plugin-transform-existence/src/index.js
+++ compiler/packages/xjs-plugin-transform-existence/src/index.js
@@ -12,10 +12,10 @@ export default declare(api => {
         const { node } = path;
         if (node.operator === '!') {
           if (t.isUnaryExpression(node.argument) && (node.argument.operator === '?')) {
-            path.replaceWith(t.binaryExpression('!=', node.argument.argument, t.nullLiteral()));
+            path.replaceWith(t.binaryExpression('==', node.argument.argument, t.nullLiteral()));
           }
         } else if (node.operator === '?') {
-          path.replaceWith(t.binaryExpression('==', node.argument, t.nullLiteral()));
+          path.replaceWith(t.binaryExpression('!=', node.argument, t.nullLiteral()));
         }
       },
     },
